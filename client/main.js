c81c3bd42249e7d5bca2b514dcf6e3a4124cd17a
denyhs <denilsonh2014@gmail.com>
2021-03-05 17:36:11 -0400

    Removing unnecessary files

--- client/main.js
+++ client/main.js
@@ -4,5 +4,5 @@ import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('root'));
+  render(<App />, document.getElementById('root'));
 });
